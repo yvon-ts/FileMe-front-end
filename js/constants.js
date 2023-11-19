3a2ebb4d9079f5611e272fb003cb33e56347bf78
yvon <>
2023-11-19 20:06:47 +0800

    test: 移除export

--- js/constants.js
+++ js/constants.js
@@ -1,6 +1,6 @@
-export const API_DOMAIN = 'https://localhost:8443';
+const API_DOMAIN = 'https://localhost:8443';
 // const API_DOMAIN = 'http://localhost:8080';
-export const API_LOGIN = API_DOMAIN + '/user/login';
+const API_LOGIN = API_DOMAIN + '/user/login';
 const API_MY_DRIVE = API_DOMAIN + '/drive/my-drive';
 const API_MY_TRASH = API_DOMAIN + '/drive/my-trash';
 const API_DRIVE_PREFIX = API_DOMAIN + '/drive/';
